c0b0439448a52e7a9afb85064b85350c910831ae
Adam Szady <adam.szady@outlook.com>
2016-11-11 13:22:41 +0100

    Added default unity elements and physics

---
+++ Pool Unity Master Game/Assets/Scripts/CueController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CueController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(moveHorizontal, 0, moveVertical)*100000);
+    }
+}
