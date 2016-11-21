85aa8837d663fdb4dc3413f7f5656a39fc1072b7
Adam Szady <adam.szady@outlook.com>
2016-11-21 17:20:51 +0100

    Cue collision detect

--- Pool Unity Master Game/Assets/Scripts/CueController.cs
+++ Pool Unity Master Game/Assets/Scripts/CueController.cs
@@ -13,21 +13,31 @@ public class CueController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-	
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+            Debug.Log("Space key was pressed.");
+
+    }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.AddForce(new Vector3(moveHorizontal, 0, moveVertical)*100);
+        rb.AddForce(new Vector3(moveHorizontal, 0, moveVertical)*1000);
     }
 
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    Debug.Log( " collected: " + collision.collider.name);
-    //    rb.AddForce(-rb.velocity);
-    //}
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log( " collected: " + collision.collider.name);
+        rb.velocity = Vector3.zero;
+    }
+
+    public void OnGUI()
+    {
+        if (Event.current.type == EventType.KeyDown)
+        {
+            Debug.Log(" pressed: " + Event.current.keyCode);
+        }
+    }
 
 }
