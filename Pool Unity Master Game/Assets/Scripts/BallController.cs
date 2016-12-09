b7ad8501dbfe75951f013e5850b939017550844d
Adam Szady <adam.szady@outlook.com>
2016-12-09 22:33:14 +0100

    Slow down and stop balls

---
+++ Pool Unity Master Game/Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LateUpdate()
+    {
+        if (_rigidbody.velocity.magnitude < 0.3 && _rigidbody.velocity.magnitude>0)
+        {
+            if (_rigidbody.velocity.magnitude > 0.03)
+            {
+                _rigidbody.velocity *= (float)0.9;
+            }
+            else
+            {
+                _rigidbody.velocity = Vector3.zero;
+                _rigidbody.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
