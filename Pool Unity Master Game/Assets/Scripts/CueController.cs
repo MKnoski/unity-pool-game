06b725eae743d250d6c9753ea7211832aae05c21
kopu <konradpuchalski93@gmail.com>
2017-01-10 20:25:54 +0100

    refaktor w sterowaniu

--- Pool Unity Master Game/Assets/Scripts/CueController.cs
+++ Pool Unity Master Game/Assets/Scripts/CueController.cs
@@ -2,124 +2,117 @@
 
 namespace Assets.Scripts
 {
-    public class CueController : MonoBehaviour
-    {
+  public class CueController : MonoBehaviour
+  {
     private AudioSource _audioSource;
     private Rigidbody _rigidbody;
 
-        private float _moveHorizontal;
-        private float _moveVertical;
-        private float _rotateHorizontal;
-        private float _rotateVertical;
-        private float _moveUpDown;
+    private float _moveHorizontal;
+    private float _moveVertical;
+    private float _rotateHorizontal;
+    private float _rotateVertical;
+    private float _moveUpDown;
 
-        public bool ShowAxes = true;
+    public bool ShowAxes = true;
 
-        private void Start()
-        {
-            this._rigidbody = GetComponent<Rigidbody>();
-      this._audioSource = GetComponent<AudioSource>();
-        }
+    private void Start()
+    {
+      _rigidbody = GetComponent<Rigidbody>();
+      _audioSource = GetComponent<AudioSource>();
+    }
 
-        // Update is called once per frame
-        private void Update()
-        {
-            //if (Input.GetKeyDown(KeyCode.Space))
-            //{
-            //  Debug.Log("Space key was pressed.");
-            //}
-        }
+    // Update is called once per frame
+    private void Update()
+    {
 
-        private void FixedUpdate()
-        {
+    }
+
+    private void FixedUpdate()
+    {
       if (Input.GetAxis(InputAxes.HorizontalCue) != 0)
       {
-        this._moveHorizontal = Input.GetAxis(InputAxes.HorizontalCue);
+        _moveHorizontal = Input.GetAxis(InputAxes.HorizontalCue);
       }
-      if (Input.GetAxis(InputAxes.RightStickX) != 0)
+      if (Input.GetAxis(InputAxes.DirectionPadX) != 0)
       {
-        this._moveHorizontal = Input.GetAxis(InputAxes.RightStickX);
+        _moveHorizontal = Input.GetAxis(InputAxes.DirectionPadX);
       }
 
       if (Input.GetAxis(InputAxes.RotateHorizontalCue) != 0)
       {
-        this._rotateHorizontal = Input.GetAxis(InputAxes.RotateHorizontalCue);
+        _rotateHorizontal = Input.GetAxis(InputAxes.RotateHorizontalCue);
       }
-      if (Input.GetAxis(InputAxes.DirectionPadX) != 0)
+      if (Input.GetAxis(InputAxes.RightStickX) != 0)
       {
-        this._rotateHorizontal = Input.GetAxis(InputAxes.DirectionPadX);
+        _rotateHorizontal = Input.GetAxis(InputAxes.RightStickX);
       }
 
       if (Input.GetAxis(InputAxes.VerticalCue) != 0)
       {
-        this._moveVertical = Input.GetAxis(InputAxes.VerticalCue);
+        _moveVertical = Input.GetAxis(InputAxes.VerticalCue);
       }
-      if (Input.GetAxis(InputAxes.RightStickY) != 0)
+      if (Input.GetAxis(InputAxes.DirectionPadY) != 0)
       {
-        this._moveVertical = -Input.GetAxis(InputAxes.RightStickY);
+        _moveVertical = -Input.GetAxis(InputAxes.DirectionPadY);
       }
 
       if (Input.GetAxis(InputAxes.RotateVerticalCue) != 0)
       {
-        this._rotateVertical = Input.GetAxis(InputAxes.RotateVerticalCue);
+        _rotateVertical = Input.GetAxis(InputAxes.RotateVerticalCue);
       }
-      if (Input.GetAxis(InputAxes.DirectionPadY) != 0)
+      if (Input.GetAxis(InputAxes.RightStickY) != 0)
       {
-        this._rotateVertical = Input.GetAxis(InputAxes.DirectionPadY);
+        _rotateVertical = Input.GetAxis(InputAxes.RightStickY);
       }
 
-            this._moveUpDown = Input.GetAxis(InputAxes.HeightCue);
+      _moveUpDown = Input.GetAxis(InputAxes.HeightCue);
 
-            this._rigidbody.transform.Translate(new Vector3(this._moveHorizontal, this._moveUpDown, this._moveVertical) * (float)0.01);
-            this._rigidbody.transform.RotateAround(this._rigidbody.position + transform.forward, Vector3.up, this._rotateHorizontal);
-            this._rigidbody.transform.RotateAround(this._rigidbody.position + transform.forward, this.transform.right, this._rotateVertical);
-      //_rigidbody.AddForce(new Vector3(_moveHorizontal, 0, _moveVertical) * 10);
+      _rigidbody.transform.Translate(new Vector3(_moveHorizontal, _moveUpDown, _moveVertical) * (float)0.01);
+      _rigidbody.transform.RotateAround(_rigidbody.position + transform.forward, Vector3.up, _rotateHorizontal);
+      _rigidbody.transform.RotateAround(_rigidbody.position + transform.forward, transform.right, _rotateVertical);
 
       _moveHorizontal = _moveVertical = _rotateHorizontal = _rotateVertical = _moveUpDown = 0f;
-        }
-
-        private void OnDrawGizmos()
-        {
-            if (ShowAxes && this._rigidbody != null)
-            {
-                Gizmos.DrawLine(this._rigidbody.position + transform.forward + this.transform.right,
-          this._rigidbody.position + transform.forward - this.transform.right);
-                Gizmos.DrawLine(this._rigidbody.position + transform.forward + Vector3.up,
-          this._rigidbody.position + transform.forward - Vector3.up);
-                Gizmos.DrawLine(this._rigidbody.position, this._rigidbody.position + this._rigidbody.transform.forward * 10);
-            }
-        }
-
-        private void LateUpdate()
-        {
-            //if (Input.GetKeyDown(KeyCode.R))
-            //{
-            //    this._rigidbody.AddForce(this._rigidbody.transform.forward*500);
-            //}
-
-            if (Input.GetButtonDown(InputAxes.HitCue))
-            {
-                this._rigidbody.AddForce(this._rigidbody.transform.forward * 500);
-            }
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.transform.parent.name == "Balls")
-            {
-                if (!audioSource.isPlaying)
-                {
-                    audioSource.Play();
-                }
-            }
-
-            this._rigidbody.velocity = Vector3.zero;
-        }
-
-        private void OnGUI() // działa tylko przy statycznej kamerze
-        {
-            if ((Event.current.keyCode == KeyCode.Space) && (Event.current.type == EventType.KeyDown))
-                this._rigidbody.AddForce(new Vector3(0, 0, 500));
-        }
     }
+
+    private void OnDrawGizmos()
+    {
+      if (ShowAxes && _rigidbody != null)
+      {
+        Gizmos.DrawLine(_rigidbody.position + transform.forward + transform.right,
+          _rigidbody.position + transform.forward - transform.right);
+        Gizmos.DrawLine(_rigidbody.position + transform.forward + Vector3.up,
+          _rigidbody.position + transform.forward - Vector3.up);
+        Gizmos.DrawLine(_rigidbody.position, _rigidbody.position + _rigidbody.transform.forward * 10);
+      }
+    }
+
+    private void LateUpdate()
+    {
+      if (Input.GetButtonDown(InputAxes.HitCue))
+      {
+        _rigidbody.AddForce(_rigidbody.transform.forward * 500);
+      }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      if (collision.gameObject.transform.parent.name == "Balls")
+      {
+        if (!_audioSource.isPlaying)
+        {
+          _audioSource.Play();
+        }
+      }
+
+      _rigidbody.velocity = Vector3.zero;
+    }
+
+    private void OnGUI() // działa tylko przy statycznej kamerze
+    {
+      if ((Event.current.keyCode == KeyCode.Space) && (Event.current.type == EventType.KeyDown))
+      {
+        _rigidbody.AddForce(new Vector3(0, 0, 500));
+      }
+    }
+  }
 }
